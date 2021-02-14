c6d2291799c27c3837e4dc184fdfd331796c5fe9
Georgiana Dumitru <georgiana.a.dumitru@gmail.com>
2021-02-14 21:47:03 +0200

    first commit

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+  $name = $_POST['name'];
+  $mailFrom = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+$mailTo = "georgiana.a.dumitru@gmail.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have recceived an email from ".$name.".\n\n".$message;
+
+mail($mailTo, $subject, $txt, $headers);
+header("Location: contact.html");
+
+?>
